0060b1ac07e03dec05464f8454b2de088f040eb4
shahende <shahenda.mena@gmail.com>
2020-08-24 13:21:17 +0200

    Using ViewComponenet to Display UserName

---
+++ SpeedyFood/ViewComponents/UserNameViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpeedyFood.Models;
+using SpeedyFood.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace SpeedyFood.ViewComponents
+{
+    public class UserNameViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserNameViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimIdentity = (ClaimsIdentity)User.Identity;
+            var claims = claimIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            ApplicationUser applicationUser = _unitOfWork.User.Find(a => a.Id == claims.Value)
+                .FirstOrDefault();
+
+            return View(applicationUser);
+        }
+              
+    }
+}
